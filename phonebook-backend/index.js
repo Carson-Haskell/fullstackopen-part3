ba94564132b388e6d0ac4619acd0137a74812d18
Carson Haskell <carson.haskell@gmail.com>
2023-06-23 13:30:17 -0700

    uibuild

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 morgan.token("body", (res, req) => {
   if (res.method !== "POST") return "No body provided";
